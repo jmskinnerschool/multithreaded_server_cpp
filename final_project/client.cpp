42716fe3ce44bab78df81e96815a3f02830edaba
skinner <jmskinner@uchicago.edu>
2020-03-18 18:42:08 -0500

    Setting up the initial directory and cmake files in clion

---
+++ final_project/client.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jake Skinner on 3/18/20.
+//
+
+#include "client.h"
---
+++ final_project/client.h
@@ -0,0 +1,14 @@
+//
+// Created by Jake Skinner on 3/18/20.
+//
+
+#ifndef FINAL_PROJECT_CLIENT_H
+#define FINAL_PROJECT_CLIENT_H
+
+
+class client {
+
+};
+
+
+#endif //FINAL_PROJECT_CLIENT_H
